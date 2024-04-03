f483390abee773039bf99deb7f8d00f516edf487
MiguelCoelhoFarias <miguelcoelho500@gmail.com>
2024-04-02 23:20:54 -0300

    function CREATE finish, and READ too

--- main.py
+++ main.py
@@ -29,14 +29,6 @@ comandosBanco = db.cursor();
 ### alterando a tabela para criar uma chave primaria para cada dado que entrar:
 comandosBanco.execute("CREATE TABLE IF NOT EXISTS ocorrencias (id INT AUTO_INCREMENT PRIMARY KEY, tipo VARCHAR(255), assunto VARCHAR(255), descricao VARCHAR(255))")
 
-### esse comando mostra os dados que estao presentes na nossa tabela - no nosso caso a tabela de ocorrencias: 
-
-comandosBanco.execute("SELECT * FROM ocorrencias")
-resultados = comandosBanco.fetchall();
-
-for resultado in resultados:
-    print(resultado)
-
 optionsMenu = 0
 print("Bem vindo ao sistema de ouvidoria da unifacisa. By: Miguelzin ^^")
 while optionsMenu != 4:
@@ -47,20 +39,46 @@ while optionsMenu != 4:
         print("1-Elogio\n2-Sugestão\n3-Reclamação")
         optionsTicket = int(input())
         if optionsTicket == 1:
+        
             assunto= input("Digite o assunto do seu elogio: ");
             print();
-            descricao= input("Agora escreva a sua ocorrencia");
+            descricao= input("Agora escreva a sua ocorrencia: ");
             comando_sql = "INSERT INTO ocorrencias (tipo, assunto, descricao) VALUES (%s, %s, %s)"
-            dadosParaBanco = ("elogio", assunto, descricao)
-            comandosBanco.execute(comando_sql, dadosParaBanco)
-            db.commit();
+            dadosDaOcorrencia = ("elogio", assunto, descricao)
+            comandosBanco.execute(comando_sql, dadosDaOcorrencia)
+            db.commit(); # commitando as alteracoes (salvando os dados novos no banco)
             print("ocorrencia registrada com sucesso!") # aqui ele registra nossa ocorrencia
+        
         elif optionsTicket == 2:
-            print()
+        
+            assunto= input("Digite o assunto da sua sugestão: ");
+            print();
+            descricao= input("Agora escreva a sua ocorrencia: ");
+            comando_sql = "INSERT INTO ocorrencias (tipo, assunto, descricao) VALUES (%s, %s, %s)"
+            dadosDaOcorrencia = ("sugestao", assunto, descricao)
+            comandosBanco.execute(comando_sql, dadosDaOcorrencia)
+            db.commit(); # commitando as alteracoes (salvando os dados novos no banco)
+            print("ocorrencia registrada com sucesso!") # aqui ele registra nossa ocorrencia
+        
         elif optionsTicket == 3:
-            print()
+            
+            assunto= input("Digite o assunto da sua reclamacao: ");
+            print();
+            descricao= input("Agora escreva a sua ocorrencia: ");
+            comando_sql = "INSERT INTO ocorrencias (tipo, assunto, descricao) VALUES (%s, %s, %s)"
+            dadosDaOcorrencia = ("reclamacao", assunto, descricao);
+            comandosBanco.execute(comando_sql, dadosDaOcorrencia);
+            db.commit(); # commitando as alteracoes (salvando os dados novos no banco)
+            print("ocorrencia registrada com sucesso!"); # aqui ele registra nossa ocorrencia
+
     elif optionsMenu == 2:
-        print("aqui estao os tickets salvos em nosso sistema:")
+        
+        ### esse comando mostra os dados que estao presentes na nossa tabela - no nosso caso a tabela de ocorrencias: 
+        comandosBanco.execute("SELECT * FROM ocorrencias")
+        resultados = comandosBanco.fetchall();
+
+        for resultado in resultados:
+            print(resultado)
 
     elif optionsMenu == 3:
         print("Deseja apagar: ")
